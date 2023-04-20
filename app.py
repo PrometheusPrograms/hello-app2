6d643f6c80532e175b0f612a0979ccc0d05ab25b
yuriyp@gmail.com <yuriyp@gmail.com>
2023-04-20 01:29:00 -0400

    update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 @app.route('/update', methods=['POST'])
 def webhook():
         if request.method == 'POST':
-            repo = git.Repo('./hello-app2')
+            repo = git.Repo('https://github.com/PrometheusPrograms/hello-app2.git')
             origin = repo.remotes.origin
             origin.pull()
             return 'Updated PythonAnywhere successfully', 200
